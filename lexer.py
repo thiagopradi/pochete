cade5beaaa5497c8138334aa02955be5ad0d9c45
Thiago Pradi <tchandy@gmail.com>
2010-10-20 19:09:09 -0200

    Atualizando parser

--- lexer.py
+++ lexer.py
@@ -4,7 +4,11 @@ import os
 import ply.lex as lex
 from ply.lex import TOKEN
 
-tokens = ('ID','RESERVADO','INTEIRO', 'BINARIO', 'OCTAL', 'HEXADECIMAL', 'REAL', 'LITERAL', 'SIMBOLO')
+palavras_reservadas = {'and':'AND', 'def':'DEF', 'elif':'ELIF','else':'ELSE',
+		'false':'FALSE', 'if':'IF','input':'INPUT', 'not':'NOT', 'or':'OR', 'output':'OUTPUT', 'true':'TRUE', 'while':'WHILE'}
+
+tokens = ['ID','RESERVADO','INTEIRO', 'BINARIO', 'OCTAL', 'HEXADECIMAL', 'REAL', 'LITERAL',
+          'SIMBOLO', 'SIM_EQ', 'SIM_ATTR', 'SIM_POT', 'SIM_GE', 'SIM_LE', 'SIM_DIF'] + list(palavras_reservadas.values())
 
 maiuscula = r'[A-Z]'
 minuscula = r'[a-z]'
@@ -15,23 +19,29 @@ aux_id = r'%s(%s %s?)*|%s(%s?%s)*%s?' % (maiuscula, minuscula_digito, maiuscula,
 aux_literal = r'"[^"]*"' + r"|'[^\r\n]*'"
 new_line = (r"\r\n" if os.name == 'posix' else r"\r\n")
 
-t_SIMBOLO = r'\(|\)|\[|\]|,|;|:=|==|:|!=|<|<=|>|>=|\+|-|\*\*|\*|/|&|%'  
+#t_SIMBOLO = r'\(|\)|\[|\]|,|;|:=|==|:|!=|<|<=|>|>=|\+|-|\*\*|\*|/|&|%'  
+t_SIM_EQ = r'=='
+t_SIM_ATTR = r':='
+t_SIM_POT = r'\*\*'
+t_SIM_GE = r'>='
+t_SIM_LE = r'<='
+t_SIM_DIF = r'=='
 t_INTEIRO = r'0|%s%s*' % (positivo, digito)
-t_ignore = ' \t'
+t_ignore = ' \t\n'
+
+literals = [';', ':', ',', '[', ']', '(', ')', '+', '-', '*']
 
 
 def t_ignore_COMMENT(t):
-  r'\#.*\r\n'
+  r'\#.*\n'
   t.lexer.lineno += 1
   pass
 
 @TOKEN(aux_id)
 def t_ID(t):
   r'aux_id'
-  palavras_reservadas = ['and', 'def', 'elif','else','false', 'if','input', 'not', 'or', 'output', 'true', 'while']
   if t.value in palavras_reservadas:
-    t.type = 'RESERVADO'
-  
+    t.type = palavras_reservadas.get(t.value, 'ID')
   return t
 
 def t_BINARIO(t):
@@ -59,11 +69,13 @@ def t_LITERAL(t):
   return t
 
 def t_error(t):
-  if(t.value.strip()[0] in ['"', "'"]):
+  if(t.value.strip() and t.value.strip()[0] in ['"', "'"]):
     raise Exception(u"Erro na linha %s - constante literal não finalizada" % t.lexer.lineno)
   else:
+    print t
     raise Exception(u"Erro na linha %s - %s - símbolo inválido" % (t.lexer.lineno, t.value[0]))
-
+  
+  
 @TOKEN(new_line)
 def t_newline(t):
   r'new_line'
--- parser.py
+++ parser.py
@@ -1,118 +1,178 @@
+# coding: utf-8
 import ply.yacc as yacc
 from lexer import tokens, lexer
 
 def p_programa(p):
-    'def identificador : [<lista de comandos>]'
+    "programa : DEF ID ':' '[' listacmd ']'"
+    pass
+
+def p_empty(p):
+    "empty :"
     pass
     
 def p_listacmd(p):
-    ' <listacmd> ::= <comando> <listacmd1>'
+    "listacmd : comando listacmd1"
     pass
     
 def p_listacmd1(p):
-    ' <listacmd1> ::=  | <lista de comandos>'
+    """listacmd1 : empty 
+               | listacmd"""
     pass
     
 def p_comando(p):
-    ' <comando> ::= <cmdatribui> | <cmdentrada> | <cmdsaida> | <cmdselecao> | <cmdrepeticao>'
+    """comando : cmdatribui
+               | cmdentrada
+               | cmdsaida
+               | cmdselecao
+               | cmdrepeticao"""
     pass
 
 def p_listaidenti(p):
-    ' <listaidenti> ::= identificador <listaindenti1>'
+    "listaidenti : ID listaindenti1"
     pass
 
 def p_listaindenti1(p):
-    ' <listaindenti1> ::=  | <lista de identificadores>'
+    """listaindenti1 : empty
+                     | ',' listaidenti"""
     pass
 
 def p_listaexp(p):
-    ' <listaexp> ::= <expressão> <listaexp1>'
+    "listaexp : expressao listaexp1"
     pass
 
 def p_listaexp1(p):
-    ' <listaexp1> ::= , <listaexp> | '
+    """listaexp1 : ',' listaexp
+                 | empty """
     pass
 
 def p_cmdatribui(p):
-    ' <cmdatribui> ::= <lista de identificadores> := <expressão>;'
+    "cmdatribui : listaidenti SIM_ATTR expressao ';'"
     pass
 
 def p_cmdentrada(p):
-    ' <cmdentrada> ::= input( <lista de identificadores> );'
+    "cmdentrada : INPUT '(' listaidenti ')' ';'"
     pass
 
 def p_cmdsaida(p):
-    ' <cmdsaida> ::= output( <listaexp> );'
+    "cmdsaida : OUTPUT '(' listaexp ')' ';'"
     pass
 
 def p_cmdselecao(p):
-    ' <cmdselecao> ::= if <expressão> : [ <lista de comandos>] <elif> <else>;'
+    "cmdselecao : IF expressao ':' '[' listacmd ']' elif else ';'"
     pass
     
 def p_elif(p):
-    ' <elif> ::=  | elif <expressão> : [ <lista de comandos> ] <elif>'
+    """elif : empty 
+            | ELIF expressao ':' '[' listacmd ']' elif"""
     pass
 
 def p_else(p):
-    ' <else> ::=  | else: [ <lista de comandos> ]'
+    """else : empty
+            | ELSE ':' '[' listacmd ']'"""
     pass
 
 def p_cmdrepeticao(p):
-    ' <cmdrepeticao> ::= while <expressão> : [ <lista de comandos>] <else>;'
+    "cmdrepeticao : WHILE expressao ':' '[' listacmd ']' else ';'"
     pass
 
 def p_expressao(p):
-    ' <expressao> ::= <valor> <expressão1>'
+    "expressao : valor expressao1"
     pass
 
 def p_expressao1(p):
-    ' <expressao1> ::=  | or <valor> <expressão1> | and <valor> <expressão1>'
+    """expressao1 : empty
+                | OR valor expressao1
+                | AND valor expressao1"""
     pass
 
 def p_valor(p):
-    ' <valor> ::= <relacional> | true | false | not <valor>'
+    """valor : relacional
+           | TRUE
+           | FALSE
+           | NOT valor"""
     pass
 
 def p_relacional(p):
-    ' <relacional> ::= <aritmética> <relacional1>'
+    "relacional : aritmetica relacional1"
     pass
 
 def p_relacional1(p):
-    ' <relacional1> ::= <operador> <aritmética> | '
+    """relacional1 : operador aritmetica
+                 | empty"""
     pass
 
 def p_operador(p):
-    ' <operador> ::= == | != | < | <= | > | >='
+    """operador : SIM_EQ
+              | SIM_DIF
+              | '<'
+              | SIM_LE
+              | '>'
+              | SIM_GE"""
     pass
 
 def p_aritmetica(p):
-    ' <aritmetica> ::= <termo> <aritmética1>'
+    "aritmetica : termo aritmetica1"
     pass
 
 def p_aritmetica1(p):
-    ' <aritmetica1> ::=  | + <termo> <aritmética1> | - <termo> <aritmética1>'
+    """aritmetica1 : empty
+                   | '+' termo aritmetica1
+                   | '-' termo aritmetica1"""
     pass
 
 def p_termo(p):
-    ' <termo> ::= <fator> <termo1>'
+    "termo : fator termo1"
     pass
 
 def p_termo1(p):
-    ' <termo1> ::=  | * <fator> <termo1> | / <fator> <termo1> | % <fator> <termo1>'
+    """termo1 : empty
+              | '*' fator termo1
+              | '/' fator termo1
+              | '%' fator termo1"""
     pass
 
 def p_fator(p):
-    ' <fator> ::= <elemento> <fator1>'
+    "fator : elemento fator1"
     pass
 
 def p_fator1(p):
-    ' <fator1> ::=  | ** <elemento> <fator1>'
+    """fator1 : empty
+              | SIM_POT elemento fator1"""
     pass
 
 def p_elemento(p):
-    ' <elemento> ::= identificador | constante_inteira | constante_real | constante_literal | ( <expressão> ) | + <elemento> | - <elemento>'
+    """elemento : ID
+                | INTEIRO
+                | REAL
+                | LITERAL
+                | '(' expressao ')'
+                | '+' elemento
+                | '-' elemento"""
     pass
 
-
-
-
+parser = yacc.yacc()
+s = """def teste: [
+		output(1);
+		input(a, b);
+		output(a);
+		y := 1 + 2;
+		ba := 32 * 33;
+		if a : [
+		  ba := ba + 3;
+		] elif a : [
+		  ba := ba+3;
+		] else: [
+		  ba := ba+3;
+		];
+		
+		while a: [
+		  ba:= ba+3;
+		] else : [
+		  ba := ba+3;
+		];
+		
+		#else: [
+		#  bff := ff+2;
+		#];
+	]"""
+print parser.parse(s, lexer=lexer())
